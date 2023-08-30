78ef0e6586192f9d7e31b6514967b680fdea9d03
Sebastian <sa@consortio.dk>
2023-08-31 00:47:13 +0200

    Opgave b

--- RealEstateApp/ViewModels/BarometerPageViewModel.cs
+++ RealEstateApp/ViewModels/BarometerPageViewModel.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using RealEstateApp.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace RealEstateApp.ViewModels
 {
@@ -21,9 +25,27 @@ namespace RealEstateApp.ViewModels
         }
 
         private double _currentAlt;
-        public double CurrentAlt { get => _currentAlt; set {SetProperty(ref _currentAlt, value); } }
+        public double CurrentAlt { get => _currentAlt; set { SetProperty(ref _currentAlt, value); } }
         private double _currentPressure;
-        public double CurrentPressure { get => _currentPressure; set {SetProperty(ref _currentPressure, value); } }
+        public double CurrentPressure { get => _currentPressure; set { SetProperty(ref _currentPressure, value); } }
+
+        private string _label;
+        public string Label { get => _label; set {SetProperty(ref _label, value); } }
+
+
+        public ObservableCollection<BarometerMeasurement> BarometerCollection { get; } = new();
+        private Command saveBarometerCommand;
+        public ICommand SaveBarometerCommand => saveBarometerCommand ??= new Command(() => SaveCurrent());
+        private void SaveCurrent()
+        {
+            var newBar = new BarometerMeasurement() { Altitude = CurrentAlt, Pressure = CurrentPressure, Label =  this.Label };
+            if (BarometerCollection.Count > 0)
+            {
+                var prevBar = BarometerCollection.Last();
+                newBar.HeightChange = CurrentAlt - prevBar.Altitude;
+            }
+            BarometerCollection.Add(newBar);
+        }
 
     }
 }
